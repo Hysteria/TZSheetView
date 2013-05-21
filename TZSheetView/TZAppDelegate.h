4e8caf8b7dfb7f3e57651d32211a528374e0fe31
ZhouHangqing <zhouhangqing@gmail.com>
2013-05-21 15:21:59 +0800

    Initial Commit

---
+++ TZSheetView/TZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TZAppDelegate.h
+//  TZSheetView
+//
+//  Created by Zhou Hangqing on 13-5-21.
+//  Copyright (c) 2013年 Zhou Hangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TZSheetView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TZSheetView
+//
+//  Created by Zhou Hangqing on 13-5-21.
+//  Copyright (c) 2013年 Zhou Hangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TZAppDelegate class]));
+    }
+}
